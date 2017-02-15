f270d4802a965a87f272dca437cde3330a72b5f5
Hannah Deason <hdeason@caxy.com>
2017-02-14 21:37:49 -0600

    Initial commit of hannah deason theme port from wix

--- wp-content/themes/hannahdeason/functions.php
+++ wp-content/themes/hannahdeason/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'avada-stylesheet' ) );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'avada-stylesheet', 'avada-dynamic-css' ) );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
